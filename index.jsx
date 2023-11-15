b00cdcbca06ee92662e16d46c44708f96afc86c9
Shadrack Kimaau <musyokishadrach1@gmail.com>
2023-11-15 11:07:02 +0300

    upadting to latest version

--- index.jsx
+++ index.jsx
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom"
 
 import App from "./App"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
