56e42547a810a35cd2af056e1929858af3a7ba63
Shadrack Kimaau <musyokishadrach1@gmail.com>
2023-11-15 11:21:15 +0300

    bug fix

--- index.jsx
+++ index.jsx
@@ -1,11 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom";
 import App from "./App"
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>
+   </StrictMode>
 );
